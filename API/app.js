7c53c44dd62995df0ee8be1a87aba9311dc661ab
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-27 23:21:09 +0530

    api v1

---
+++ API/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json' )) 
+
+app.get('/api/v1/tours', (req,res)=>{
+  res.json({
+    status: 'success',
+    results: tours.length,
+    data: {
+        tours:tours
+    }
+  })
+})
+
+app.post('/api/v1/tours', (req,res)=>{
+//   console.log(req.body);
+  const newId = tours[tours.length-1].id + 1;
+  const newTour = Object.assign({id: newId}  , req.body);
+  tours.push(newTour);
+  fs.writeFile('./dev-data/data/tours-simple.json',JSON.stringify(tours) , err=>{
+    res.status(201).json({
+        status:'success',
+        data:{
+            tour:newTour
+        }
+    })
+  })
+});
+
+const port =3000;
+app.listen(port , ()=>{
+    console.log(`App running on port ${port}`);
+})
